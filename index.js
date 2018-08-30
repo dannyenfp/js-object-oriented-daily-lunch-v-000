98f9a4e63a3e659a1099d94c10fe33bee8b2f595
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-30 22:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ class Customer {
   
 //maybe use reduce here   
   totalSpent() {
-    return this.meals().reduce(((acc, totalVal) => acc + totalVal), 0);
+    return this.meals().reduce(((acc, meal) => acc + meal.price), 0);
   }
 }
 
